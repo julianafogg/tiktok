e3b5f4e43785305ac20080d7baf30219673245e4
Juliana Fogg <julianafogg@codeandtheory.com>
2022-03-24 17:55:56 -0400

    add threeup

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -3,13 +3,18 @@ import "./_card.scss"
 
 const Card = ({content}) => {
 
+    console.log(content)
+
   return(
     <div className="card">
+  
         <div className="card__image">
             {content.image && <img src={content.image.src} alt={content.image.alt} />}
             <div className="card__body">
+               <p>{content.eyebrow}</p>
+                <h2>{content.heading}</h2>
                 <h4>{content.title}</h4>
-                <p>{content.body}</p>
+                <h6 >{content.body}</h6>
                 <a href='#' className="card__button">{content.button}</a>
             </div>
         </div> 
--- src/pages/index.js
+++ src/pages/index.js
@@ -15,14 +15,50 @@ export default function Homepage() {
       list: {
         items: [
           {
-            title: "Want to try something new together?",
+            heading: "Want to try something new together?",
             body: "Lets get started.",
             button: "Create now",
           },
         ]
       },
-    }
-  };
+
+      threeUp: [  
+        {
+          image: {
+            src: "https://thumbnails.production.thenounproject.com/B2C16BR4WIEfKiW7Bc9QuZYExI4=/fit-in/1000x1000/photos.production.thenounproject.com/photos/921AB49F-C5EB-43D3-8F14-6CCA6628C8F7.jpg",
+            alt: "icecream"
+          },
+          eyebrow: "Solutions",
+          title: "TopView",
+          body: "Greet your audience as soon as they open the app with placements that show up before any other content.",
+          button: "Create now",
+        },
+        {
+          image: {
+            src: "https://thumbnails.production.thenounproject.com/1PUZr344pmemQ_EN1CuCBSHjUUs=/fit-in/1000x1000/photos.production.thenounproject.com/photos/E577017E-405A-475D-9DF9-0AACFCF7F107.jpg",
+            alt: "icecream"
+          },
+          eyebrow: "Solutions",
+          title: "In-Feed Ads",
+          body:"Reach users in their feeds alongside native TikTok content with fully immersive ads.",
+          button: "Create now",
+        },
+        {
+          image: {
+            src: "https://thumbnails.production.thenounproject.com/OPErbEDuKjPYHec7Ds1aIkFXFPQ=/fit-in/1000x1000/photos.production.thenounproject.com/photos/DD53C344-3166-426F-9518-F65B12D24A2C.jpg",
+            alt: "icecream"
+          },
+          eyebrow: "Solutions",
+          title: "Branded Hashtag Challenge",
+          body: "Engage with the TikTok community by starting a new trend using a hashtag bespoke to your brand.",
+          button: "Create now",
+        },
+      ],
+
+
+  }  
+    
+};
 
 
   return (
